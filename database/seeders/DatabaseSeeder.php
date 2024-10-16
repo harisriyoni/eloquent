b7def5bc15fd18d696cafcdda56eb2e8731ea1fb
Haris Riyoni <1214047@std.ulbi.ac.id>
2024-10-16 10:53:08 +0700

    7

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,21 +3,16 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Customer;
+use App\Models\Wallet;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        $this->call(CategorySeeder::class);
+        // $this->call(CategorySeeder::class);
+        $this->call(ProductSeeder::class);
     }
 }
