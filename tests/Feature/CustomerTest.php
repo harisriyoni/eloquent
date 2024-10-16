732e820252903c0b173c6111934b6356819af761
Haris Riyoni <1214047@std.ulbi.ac.id>
2024-10-16 14:47:05 +0700

    cust test

--- tests/Feature/CustomerTest.php
+++ tests/Feature/CustomerTest.php
@@ -10,29 +10,37 @@ use Tests\TestCase;
 
 class CustomerTest extends TestCase
 {
-    public function testHasOne()
-    {
+    // public function testHasOne()
+    // {
+    //     $customer = Customer::query()->find("1");
+    //     assertNotNull($customer);
+    //     $wallet = $customer->wallet;
+    //     assertNotNull($wallet);
+    //     assertNotNull(10000, $wallet->amount);
+    //     assertNotNull(1, $wallet->customer_id);
+    //     assertNotNull(3, $wallet->id);
+
+    //     // $amount = $wallet->amount;
+    //     // assertNotNull(10000, $amount);
+    // }
+    // public function testOneToOne(){
+    //     $customer = new Customer();
+    //     $customer->id = 2;
+    //     $customer->name = "riyoni";
+    //     $customer->email = "riyoni@gmail.com";
+    //     $customer->save();
+
+    //     $wallets = new Wallet();
+    //     $wallets->amount = 100000;
+    //     $customer->wallet()->save($wallets);
+
+    // }
+
+    public function testOneThourght(){
         $customer = Customer::query()->find("1");
         assertNotNull($customer);
-        $wallet = $customer->wallet;
-        assertNotNull($wallet);
-        assertNotNull(10000, $wallet->amount);
-        assertNotNull(1, $wallet->customer_id);
-        assertNotNull(3, $wallet->id);
-
-        // $amount = $wallet->amount;
-        // assertNotNull(10000, $amount);
-    }
-    public function testOneToOne(){
-        $customer = new Customer();
-        $customer->id = 2;
-        $customer->name = "riyoni";
-        $customer->email = "riyoni@gmail.com";
-        $customer->save();
-
-        $wallets = new Wallet();
-        $wallets->amount = 100000;
-        $customer->wallet()->save($wallets);
-
+        $va = $customer->virtualAccount;
+        self::assertNotNull($va);
+    //     self::assertEquals("BCA", $va->bank);
     }
 }
