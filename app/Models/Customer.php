42bd470416e5c92aae2abea69e7a62dde1303e61
Haris Riyoni <1214047@std.ulbi.ac.id>
2024-10-16 14:02:20 +0700

    customer

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 
 class Customer extends Model
 {
@@ -16,5 +17,13 @@ class Customer extends Model
     public function wallet(): HasOne
     {
         return $this->hasOne(Wallet::class, "customer_id", "id");
+    }
+    public function virtual_account(): HasOneThrough{
+        return $this->hasOneThrough([Wallet::class, VirtualAccount::class],
+        "customer_id", //fk on wallets table
+        "wallet_id", //fk on virtual account table
+        "id", // pk on costumer table
+        "id", //pk on wallet table
+    );
     }
 }
