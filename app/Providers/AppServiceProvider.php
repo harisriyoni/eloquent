2cfd0e5d769859bf1666a762c866e1e46a89aad3
Haris Riyoni <1214047@std.ulbi.ac.id>
2024-10-21 15:15:40 +0700

    polymorphic type

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\Customer;
+use App\Models\Product;
+use App\Models\Voucher;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +25,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        DB::listen(function ($query) {
+            Log::info("Query: {$query->sql}");
+        });
+        Relation::enforceMorphMap([
+            'product' => Product::class,
+            'voucher' => Voucher::class,
+            'customer' => Customer::class,
+        ]);
     }
 }
--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -17,7 +17,7 @@ class CommentSeeder extends Seeder
         $comment->title = "Title";
         $comment->comment = "aku comment di product";
         $comment->commentable_id = $product->id;
-        $comment->commentable_type = Product::class;
+        $comment->commentable_type = 'product';
         $comment->save();
 
         $voucher = Voucher::query()->first();
@@ -26,7 +26,7 @@ class CommentSeeder extends Seeder
         $comment->title = "Title";
         $comment->comment = "aku comment di voucher";
         $comment->commentable_id = $voucher->id;
-        $comment->commentable_type = Voucher::class;
+        $comment->commentable_type = 'voucher';
         $comment->save();
     }
 }
