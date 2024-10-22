94c7c7485ec4984823f1008d0fc53bd8c209f36d
Haris Riyoni <1214047@std.ulbi.ac.id>
2024-10-22 15:28:23 +0700

    eloquent collection

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -4,28 +4,28 @@ namespace Tests\Feature;
 
 use App\Models\Category;
 use App\Models\Product;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
-
 use function PHPUnit\Framework\assertCount;
+use function PHPUnit\Framework\assertEquals;
 use function PHPUnit\Framework\assertNotNull;
+use Tests\TestCase;
 
 class ProductsTest extends TestCase
 {
-    public function testOneToMany(){
+    public function testOneToMany()
+    {
         $products = Product::query()->find("1");
         assertNotNull($products);
         $products->category_id;
 
     }
 
-    public function testManyofManyPolymorphic(){
+    public function testManyofManyPolymorphic()
+    {
         $products = Product::query()->first();
         $tags = $products->tags;
         assertNotNull($tags);
         // assertCount(1, $tags);
-        foreach($tags as $tag){
+        foreach ($tags as $tag) {
             assertNotNull($tag);
             assertNotNull($tag->id);
             assertNotNull($tag->name);
@@ -37,5 +37,20 @@ class ProductsTest extends TestCase
         assertNotNull($category);
         $products = $category->products()->where('price', '=', 100)->get();
         assertCount(1, $products);
+    }
+    public function testQueryAggregate()
+    {
+        $category = Category::query()->find("FOOD");
+        assertNotNull($category);
+        $productCount = $category->products()->count();
+        $this->assertEquals(5, $productCount);
+    }
+    public function testCollection()
+    {
+        $product = Product::query()->get();
+        assertCount(5, $product);
+
+        $product = $product->toQuery()->where('price', 100)->get();
+        assertEquals("1", $product[0]->id);
     }
 }
